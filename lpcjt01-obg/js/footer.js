93166a40e9e3ba3f54d8d7b50131dc7a71201f41
jotainvest <108202340+jotainvest@users.noreply.github.com>
2024-11-09 00:13:44 -0300

    Add files via upload

---
+++ lpcjt01-obg/js/footer.js
@@ -0,0 +1,7 @@
+var date = new Date(),
+    year = date.getFullYear();
+$(".footer").append(
+    '<img src="https://pub-c6bb9e415a0842389ae10388ba2d1cd4.r2.dev/ota.svg" alt="" class="logoJota"><p>Todo o material está protegido pela Lei de Direitos Autorais e é de uso exclusivo de seu destinatário, sendo vedada a sua reprodução ou distribuição, seja no todo ou em parte, sem prévia e expressa autorização do Jota, sob pena de sanções nas esferas cível e criminal.</p><p>Copyright © ' +
+    year +
+    "  - Todos os direitos reservados.</p>"
+);
